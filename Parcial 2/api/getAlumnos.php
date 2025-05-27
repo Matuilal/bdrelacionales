788ca4f5f1f1f6cef3075189edb16434ecc7411c
Matuilal <mateo.ibar09@gmail.com>
2025-05-26 19:12:28 -0600

    26/05/25 Practica 5 api getAlumnos

---
+++ Parcial 2/api/getAlumnos.php
@@ -0,0 +1,13 @@
+<?php
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+
+    $usuario=[
+        ["id" => 1, "nombre" => "Mateo Ibarra", "correo" => "mateo@gmail.com"],
+        ["id" => 1, "nombre" => "Isaac Moreno", "correo" => "isaac@gmail.com"],
+        ["id" => 1, "nombre" => "Jesus Angel", "correo" => "jesus@gmail.com"],
+    ];
+
+    echo json_encode($usuarios);
+
+?>
